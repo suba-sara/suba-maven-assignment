4320e0433c718973c2d26e140cce7a025e734d83
Subalakshmi S <subavasagan@gmail.com>
2021-08-21 08:40:18 +0530

    Initial checkin

---
+++ src/main/java/com/suba/assignment/suba_maven_assignment/App.java
@@ -0,0 +1,13 @@
+package com.suba.assignment.suba_maven_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to maven assignment suba!" );
+    }
+}
